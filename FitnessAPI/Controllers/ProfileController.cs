5b754300f72fbd7017d08bfe60892b372b5e34f4
Michał Pilch <mp300455@student.polsl.pl>
2023-05-29 15:05:40 +0200

    Created/deleted files

---
+++ FitnessAPI/Controllers/ProfileController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FitnessAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfileController : Controller
+    {
+        // GET
+        // GET
+        [HttpGet]
+        public IEnumerable<string> Get() {
+            return new List<string>();
+        }
+        
+        //
+        [HttpPost]
+        public void Post([FromBody] string value) {
+            
+        }
+        
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value) {
+        }
+        
+        [HttpDelete("{id}")]
+        public void Delete(int id) {
+            
+        }
+    }
+}
+
